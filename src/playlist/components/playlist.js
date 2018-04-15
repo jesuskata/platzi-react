7a27d71500e42288cc02c2eef90d738331bb0461
jesuskata <rzarate.alex@gmail.com>
2018-04-15 02:31:18 -0500

    Se hizo el reto de pasar todas las playlist de las 3 categorías que contiene la api.json

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -3,20 +3,25 @@ import Media from './media';
 import './playlist.css';
 
 class Playlist extends Component {
-    render() {
-        const playlist = this.props.data.categories[0].playlist;
-        console.log(this.props.data);
-        return (
-            <div className="Playlist">
-                {
-                    playlist.map((item) => { // El método map() llama a la función proporcionada una vez para cada elemento en una matriz, en orden.
-                        // return <Media title={item.title} key={item.id} /> // con esto, estamos llamando el título para Media según ES5
-                        return <Media {...item} key={item.id} />
-                    })
-                }
+  render() {
+    const playlist = this.props.data.categories;
+    console.log(this.props.data);
+    return (
+      <div>
+        {
+          playlist.map((item) => {
+            return <div className="Playlist" key={item.id}>
+              {
+                item.playlist.map((item) => {
+                  return <Media {...item} key={item.id} />
+                })
+              }
             </div>
-        )
-    }
+          })
+        }
+      </div>
+    )
+  }
 }
 
 export default Playlist;
