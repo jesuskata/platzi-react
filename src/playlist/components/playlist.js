791241f5dba174dfd5e17ee7e7ed8afce97962c7
jesuskata <rzarate.alex@gmail.com>
2018-04-18 18:54:39 -0500

    Se crearon botones para el control del visualizador de videos, como parte de una serie de componentes reutilizables

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Categorias from './categorias';
 import Media from './media';
 import './playlist.css';
+import Play from '../../icon/components/play';
+import Pause from '../../icon/components/pause';
+import Full from '../../icon/components/full-screen';
+import Volume from '../../icon/components/volume';
 
 function Playlist(props) { // en este caso estamos usando un componente funcional porque este no necesita ciclo de vida, sólo vamos a renderizar UI
   const playlist = props.data.categories;
@@ -12,6 +16,12 @@ function Playlist(props) { // en este caso estamos usando un componente funciona
         playlist.map((item) => {
           return(
             <div key={item.id} >
+              <ul>
+                <li><Play size={25} color="#053c5e" /></li>
+                <li><Pause size={25} color="#053c5e" /></li>
+                <li><Full size={25} color="#053c5e" /></li>
+                <li><Volume size={25} color="#053c5e" /></li>
+              </ul>
               <Categorias {...item} />
               <div className="Playlist">
                 {
