e58a2f65c853cfc27324cb18d5e4e87513a33fb7
jesuskata <rzarate.alex@gmail.com>
2018-04-25 12:36:54 -0500

    Hemos creado nuestro primer portal (basados en la versión 16 de ReactJS) que nos permite agregar componentes en el DOM pero en un área diferente del div inicial llamado app

--- src/entries/home.js
+++ src/entries/home.js
@@ -5,10 +5,10 @@ import data from '../api.json';
 // como nota adicional, aquí también usamos el poder de ES6 para importar { render } y usarlo sin ReactDOM.render
 // import Playlist from './src/playlist/components/playlist';
 
-const app = document.getElementById('app')
+const homeContainer = document.getElementById('home-container')
 
 // ReactDOM.render(qué voy a renderizar, dónde renderizar)
 // const holaMundo = <h1>Hola Jesús Romero</h1>
-render(<Home data={data} />, app); // < > se usa para que ReactDOM reconozca nuestro componente
+render(<Home data={data} />, homeContainer); // < > se usa para que ReactDOM reconozca nuestro componente
 
 // console.log("Hola mundo!");
