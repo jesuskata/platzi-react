ba09c8a233e4d7002b5553c50712e6b43fd0553a
jesuskata <rzarate.alex@gmail.com>
2018-04-25 16:15:23 -0500

    Hemos creado un manejador de errores según el estado del componente. Esto para validar si todo está en orden, y en caso de que no, nuestro componente handle-error se encarga de hacérnoslo saber

---
+++ src/error/components/regular-error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RegularError(props) {
+  return(
+    <div>
+      <h1 style={{color: 'white'}}>OH CRAP, there is something wrong</h1>
+    </div>
+  )
+}
+
+export default RegularError;
