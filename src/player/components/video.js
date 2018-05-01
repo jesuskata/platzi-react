db4a34dff08b6a629ae098871dfb8097ba3bea59
jesuskata <rzarate.alex@gmail.com>
2018-05-01 13:58:15 -0500

    Se tiene ahora una funcionalidad adicional, que es tener el timer del video actualizado conforme va a vanzando

--- src/player/components/video.js
+++ src/player/components/video.js
@@ -18,7 +18,7 @@ class Video extends Component {
     this.video = element;
   }
   render() {
-    const {handleLoadedMetadata} = this.props;
+    const {handleLoadedMetadata, handleTimeUpdate} = this.props;
     return(
       <div className="Video">
         <video
@@ -26,6 +26,7 @@ class Video extends Component {
           src={this.props.src}
           ref={this.setRef}
           onLoadedMetadata={handleLoadedMetadata}
+          onTimeUpdate={handleTimeUpdate}
         />
       </div>
     )
